464fbfcc0aa879f4a3602ba29c94ad2f4b7c3e48
denis bondarenko <denisamigoua@gmail.com>
2020-12-31 12:54:04 +0100

    Fix linter errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 
+const normalizeValue = (val) => {
+  if (_.isPlainObject(val)) return '[complex value]';
+  if (_.isBoolean(val) || _.isNull(val)) return val;
+  return `'${val}'`;
+};
+
 const genString = (action) => (propPath, value = '', oldValue = '') => {
-	const val = _.isPlainObject(value) ? '[complex value]' :
-		_.isBoolean(value) || _.isNull(value) ? value : `'${value}'`;
-	const oldVal = _.isPlainObject(oldValue) ? '[complex value]' :
-		_.isBoolean(oldValue) || _.isNull(oldValue) ? oldValue : `'${oldValue}'`;
+  const val = normalizeValue(value);
+  const oldVal = normalizeValue(oldValue);
   switch (action) {
     case 'added':
       return `Property '${propPath}' was ${action} with value: ${val}`;
@@ -43,20 +47,20 @@ const actions = {
 };
 
 const isBouthComplex = (obj1, obj2, path) => {
-	const isComplex1 = _.isPlainObject(_.get(obj1, path));
-	const isComplex2 = _.isPlainObject(_.get(obj2, path));
-	return isComplex1 && isComplex2;
+  const isComplex1 = _.isPlainObject(_.get(obj1, path));
+  const isComplex2 = _.isPlainObject(_.get(obj2, path));
+  return isComplex1 && isComplex2;
 };
 
 const getAction = (o1, o2, path) => {
-	if (_.has(o2, path) && !_.has(o1, path)) return 'add';
-	if (_.has(o2, path) && _.has(o1, path)) {
-		if (!_.isEqual(_.get(o2, path), _.get(o1, path)) && !isBouthComplex(o1, o2, path)) {
-			return 'update';
-		}
-	}
-	if (!_.has(o2, path) && _.has(o1, path)) return 'remove';
-	return null;
+  if (_.has(o2, path) && !_.has(o1, path)) return 'add';
+  if (_.has(o2, path) && _.has(o1, path)) {
+    if (!_.isEqual(_.get(o2, path), _.get(o1, path)) && !isBouthComplex(o1, o2, path)) {
+      return 'update';
+    }
+  }
+  if (!_.has(o2, path) && _.has(o1, path)) return 'remove';
+  return null;
 };
 
 export default (obj1, obj2) => {
@@ -69,12 +73,12 @@ export default (obj1, obj2) => {
       const actionType = getAction(obj1, obj2, path);
       const val1 = _.get(obj1, path);
       const val2 = _.get(obj2, path);
-			if (!_.isNull(actionType)) {
-				acc.push(actions[actionType](path, val2, val1));
-				allPaths.forEach((line, i, arr) => {
-					if (line.includes(path) && line.length > path.length) arr[i] = '';
-				});
-			}
+      if (!_.isNull(actionType)) {
+        acc.push(actions[actionType](path, val2, val1));
+        allPaths.forEach((line, i) => {
+          if (line.includes(path) && line.length > path.length) allPaths[i] = '';
+        });
+      }
       return acc;
     }, []);
   return differenceStrings.join('\n');
